c5cbc51cb7ddfa73dcec305056a28c706934388e
Leonids Gurockins <leonid.gurockin@gmail.com>
2023-09-02 14:00:44 +0300

    Update config.php

--- Backend/config.php
+++ Backend/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USERNAME", "c36leoner");
-define("DB_PASSWORD", "Jata1234?");
-define("DB_DATABASE_NAME", "c36ecowatt");
+define("DB_USERNAME", "");
+define("DB_PASSWORD", "");
+define("DB_DATABASE_NAME", "");
